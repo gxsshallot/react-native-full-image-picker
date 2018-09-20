9fc0d4ea97c8ac9a5542d84cd43f8ba9462d1c59
白宇东 <summer88123@163.com>
2018-09-20 11:46:32 +0800

    配置Android原生代码

--- example/android/app/src/main/java/com/test/MainApplication.java
+++ example/android/app/src/main/java/com/test/MainApplication.java
@@ -7,6 +7,8 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import org.reactnative.camera.RNCameraPackage;
+import com.brentvatne.react.ReactVideoPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,7 +24,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new RNCameraPackage(),
+          new ReactVideoPackage()
       );
     }
 
